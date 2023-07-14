9b932461fa0b9ad1315adc8e2a36489ba811906a
cornelius <segutoncornelius@gmail.com>
2023-07-14 13:49:33 -0700

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,6 +12,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
-int is _digit(char *s);
 
 #endif
