383cdc97b2680de41aa8f69e7610c4062e0640b0
Cornelius <segutoncornelius@gmail.com>
2023-08-02 10:35:35 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,66 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
 /**
- * check_num -check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (success)
+ * main - addicion of numbers
+ * @argc: argc
+ * @argv: argv
+ * Return: 0, 1
  */
-int check_num(char *str)
-{
-	/*Declaring variables*/
-	unsigned int count;
 
-	count = 0;
-	while (count <strlen(str)) /*count string*/
-
-	{
-		if (!isdigit(str[count]) /*check if str there are digit*/
-			{
-				return (0);
-			}
-
-			count++;
-	}
-	return (1);
-}
-/**
- * main - print the name of the program
- * @argc: count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (success)
- */
 int main(int argc, char *argv[])
-
 {
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
+	int i;
+	int j;
+	int sum;
 
-	count = 1;
-	while (count < argc) /*Goes through the whole array*/
+	if (argc == 1)
 	{
-		if (check_num(argv[count]))
-		{
-			str_to_int = atoi (argv[count]; /ATOI -- > convert string to int * /
-					sum += str_to_int;
-		}
-
-		/*condition if one of the number contains symbols that are not digits*/
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		count++;
+		printf("%d\n", 0);
+		return (0);
 	}
-
-	printf("%d\n", sum); /*print sum*/
-
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
